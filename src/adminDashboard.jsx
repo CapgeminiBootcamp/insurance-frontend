2071fed4e4d120c6f61801de4f494ab372f62cbf
Shashvat <thelearningcoder2000@gmail.com>
2022-10-10 16:55:06 +0530

    added admin dashboard

---
+++ src/adminDashboard.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import {Outlet} from 'react-router-dom'
+import AdminNav from "./adminNav" 
+function AdminDashboard(){
+    return(
+        <div>
+            <section style={{backgroundColor: "#eee", minHeight: "100vh" }}>
+               
+               <div className = "container py-5">
+                <AdminNav />
+                <h1>Hello Admin!</h1>
+                <hr />
+                <Outlet />
+               </div>
+               
+                </section>
+        </div>
+    )
+}
--- src/components/updateCustomer.jsx
+++ src/components/updateCustomer.jsx
@@ -1,3 +1,5 @@
+import { ErrorMessage, Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import React, { Component } from 'react'
 import customerService from '../services/customerService';
 export default class UpdateCustomer extends Component {
@@ -117,6 +119,7 @@ export default class UpdateCustomer extends Component {
                             <h3 className="text-center">Update Customer</h3>
                             <div className="card-body">
                                 <form>
+                                 
                                 {/* <div className="form-group">
                                         <label> Customer Id: </label>
                                         <input name="Id" className="form-control"
@@ -180,4 +183,265 @@ export default class UpdateCustomer extends Component {
         )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { ErrorMessage, Formik, Form, Field } from "formik";
+// import * as Yup from "yup";
+// import React, { Component } from 'react'
+// import customerService from '../services/customerService';
+// export default class UpdateCustomer extends Component {
+
+
+//      updateSchema = Yup.object().shape({
+//         customerName: Yup.string().required("This Field cannot be empty"),
+//         customeraddress: Yup.string().required("This Field cannot be empty"),
+//         customerAge: Yup.number()
+//           .positive("Age cannot be zero")
+//           .required("This Field cannot be empty")
+//           .integer("Age cannot be a floating number"),
+//         customerSalary: Yup.number()
+//           .required("This Field cannot be empty")
+//           .positive("Salary cannot be negative"),
+//         customerContact: Yup.string()
+//           .length(10)
+//           .required("This Field cannot be empty")
+//           .matches(/^$|[6-9][0-9]{9}/, "Phone number is not valid"),
+    
+//         customerFamilyMembers: Yup.number()
+//           .required("This Field cannot be empty")
+//           .min(0, "Family members cannot be less than 0"),
+//         email: Yup.string().email().required("This Field cannot be empty"),
+//         password: Yup.string()
+//           .min(8, "password should be 8 characters long")
+//           .required("This Field cannot be empty"),
+//         customerGender: Yup.string().required("This Field cannot be empty"),
+//       });
+
+//     constructor(props) {
+
+    
+
+//         super(props)
+
+//         this.state = {
+//             id: this.props.match.params.id,
+//             cutomerName: "",
+//             customeraddress: "",
+//             customerAge: "",
+//             customerSalary: "",
+//             customerContact: "",
+//             customerGender: "",
+//             customerFamilyMembers: "",
+//             email: "",
+//             password: ""
+//         }
+//         this.changeCustomerIdHandler = this.changeCustomerIdHandler.bind(this);
+//         this.changeCutomerNameHandler = this.changeCutomerNameHandler.bind(this);
+//         this.changeCustomeraddressHandler = this.changeCustomeraddressHandler.bind(this);
+//         this.changeCustomerAgeHandler = this.changeCustomerAgeHandler.bind(this);
+//         this.changeCustomerSalaryHandler = this.changeCustomerSalaryHandler.bind(this);
+//         this.changeCustomerContactHandler = this.changeCustomerContactHandler.bind(this);
+//         this.changeCustomerGenderHandler = this.changeCustomerGenderHandler.bind(this);
+//         this.changeCustomerFamilyMembersHandler = this.changeCustomerFamilyMembersHandler.bind(this);
+//         this.changeEmailHandler = this.changeEmailHandler.bind(this);
+//         this.changePasswordHandler = this.changePasswordHandler.bind(this);
+
+
+//         this.updateCustomer = this.updateCustomer.bind(this);
+//     }
+//     componentDidMount() {
+//         customerService.getCustomerById(this.state.id).then((res) => {
+//             let customer = res.data;
+//             this.setState({
+//                 customerId: customer.customerId,
+//                 cutomerName: customer.cutomerName,
+//                 customeraddress: customer.customeraddress,
+//                 customerAge: customer.customerAge,
+//                 customerSalary: customer.customerSalary,
+//                 customerContact: customer.customerContact,
+//                 customerGender: customer.customerGender,
+//                 customerFamilyMembers: customer.customerFamilyMembers,
+//                 email: customer.email,
+//                 password: customer.password
+//             });
+//         });
+//     }
+
+//     updateCustomer = (values) => {
+//         // e.preventDefault();
+//         console.log("save pressed")
+//         let customer = { 
+//             customerId: this.state.customerId,
+//             cutomerName: values.customerName, 
+//             customeraddress:  values.customeraddress, 
+//             customerAge: values.customerAge,
+//             customerSalary:  values.customerSalary,
+//             customerContact: values.customerContact,
+//             customerGender: values.customerGender, 
+//             customerFamilyMembers: values.customerFamilyMembers,
+//             email: values.email,
+//             password: values.password};
+
+//         customerService.updateCustomer(customer).then(res => {
+//             this.props.history.push('/buyInsurance/'+ this.state.customerId);
+//         });
+//     }
+//     changeCustomerIdHandler = (event) => {
+//         this.setState({ customerId: event.target.value });
+//     }
+
+//     changeCutomerNameHandler = (event) => {
+//         this.setState({ cutomerName: event.target.value });
+//     }
+
+//     changeCustomeraddressHandler = (event) => {
+//         this.setState({ customeraddress: event.target.value });
+//     }
+
+//     changeCustomerAgeHandler = (event) => {
+//         this.setState({ customerAge: event.target.value });
+//     }
+//     changeCustomerSalaryHandler = (event) => {
+//         this.setState({ customerSalary: event.target.value });
+//     }
+//     changeCustomerContactHandler = (event) => {
+//         this.setState({ customerContact: event.target.value });
+//     }
+//     changeCustomerGenderHandler = (event) => {
+//         this.setState({ customerGender: event.target.value });
+//     }
+//     changeCustomerFamilyMembersHandler = (event) => {
+//         this.setState({ customerFamilyMembers: event.target.value });
+//     }
+//     changeEmailHandler = (event) => {
+//         this.setState({ email: event.target.value });
+//     }
+//     changePasswordHandler = (event) => {
+//         this.setState({ password: event.target.value });
+//     }
+
+//     cancel() {
+//         this.props.history.push('/buyInsurance/'+ this.state.customerId);
+//     }
+
+//     render() {
+//         return (
+
+//             <div>
+//                 <br></br>
+//                 <div className="container">
+//                     <div className="row">
+//                         <div className="card col-md-6 offset-md-3 offset-md-3">
+//                             <h3 className="text-center">Update Customer</h3>
+//                             <div className="card-body">
+//                                 <Formik
+//                                     initialValues={{
+//                                          customerId: this.state.customerId,
+//                                          cutomerName: this.state.cutomerName, 
+//                                          customeraddress: this.state.customeraddress, 
+//                                          customerAge: this.state.customerAge,
+//                                          customerSalary: this.state.customerSalary,
+//                                          customerContact: this.state.customerContact,
+//                                          customerGender: this.state.customerGender, 
+//                                          customerFamilyMembers: this.state.customerFamilyMembers,
+//                                          email: this.state.email,
+//                                          password: this.state.password
+                             
+//                                     }}
+
+//                                     onSubmit={(e) => updateCustomer(e)}
+//                                     validationSchema = {updateSchema}
+//                                     >
+//                                 {/* <div className="form-group">
+//                                         <label> Customer Id: </label>
+//                                         <input name="Id" className="form-control"
+//                                             value={this.state.customerId} onChange={this.changeCustomerIdHandler} />
+//                                     </div> */}
+//                                     {(props) => (
+//                                         <Form>
+//                                     <div className="form-group">
+//                                         <label> Customer Name: </label>
+//                                         <Field name="customerName" className="form-control" type="text" id="customerName" />
+//                                         <ErrorMessage name ="customerName" />
+//                                     </div>
+//                                     <div className="form-group">
+//                                         <label> Address: </label>
+//                                         <Field name="customeraddress" className="form-control" type="text" id="customeraddress" />
+//                                         <ErrorMessage name ="customeraddress" />
+//                                     </div>
+//                                     <div className="form-group">
+//                                         <label> Age: </label>
+//                                         <Field name="customerAge" className="form-control" type="number" id="customerAge" />
+//                                         <ErrorMessage name ="customerAge" />
+//                                     </div>
+//                                     <div className="form-group">
+//                                         <label> Salary: </label>
+//                                         <Field name="customerSalary" className="form-control" type="number" id="customerSalary" />
+//                                         <ErrorMessage name ="customerSalary" />
+//                                     </div>
+//                                     <div className="form-group">
+//                                         <label> Contact: </label>
+//                                         <Field name="customerContact" className="form-control" type="text" id="customerContact" />
+//                                         <ErrorMessage name ="customerContact" />
+//                                     </div>
+//                                     <div className="form-group">
+//                                         <label> Gender: </label>
+//                                         <Field name="customerGender" className="form-control" type="text" id="customerGender" />
+//                                         <ErrorMessage name ="customerGender" />
+//                                     </div>
+//                                     <div className="form-group">
+//                                         <label> Family Members: </label>
+//                                         <Field name="customerFamilyMembers" className="form-control" type="number" id="customerFamilyMembers" />
+//                                         <ErrorMessage name ="customerFamilyMembers" />
+//                                     </div>
+//                                     <div className="form-group">
+//                                         <label> Email: </label>
+//                                         <Field name="email" className="form-control" type="email" id="email" />
+//                                         <ErrorMessage name ="email" />
+//                                     </div>
+//                                     <div className="form-group">
+//                                         <label> Password: </label>
+//                                         <Field name="password" className="form-control" type="text" id="password" />
+//                                         <ErrorMessage name ="password" />
+//                                     </div>
+
+//                                     <button className="btn btn-outline-success" type="submit">Save</button>
+//                                     <button className="btn btn-outline-danger" onClick={this.cancel.bind(this)} style={{ margin: "1em" }}>Cancel</button>
+//                                         </Form>
+//                                     )}
+                               
+//                                 </Formik>
+//                             </div>
+//                         </div>
+//                     </div>
+
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
